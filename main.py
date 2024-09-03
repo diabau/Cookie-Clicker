de8b9f55503d01059370a00169d9013119d01110
Mathyou <andremathyou@gmail.com>
2024-09-03 23:38:32 +0200

    chore: update image file paths and save location

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from tkinter import *
 
 fenetre = Tk()
 
-fichier = open("data.txt", "a+")  # Ouvrir le fichier en mode ajout (append) ou lecture/Ã©criture (si le fichier n'existe pas, il sera crÃ©Ã©)
+fichier = open("save/data.txt", "a+")
 
-imagecookie = PhotoImage(file="cookie.png")
+imagecookie = PhotoImage(file="assets/cookie.png")
 
 fenetre.geometry("500x500")
-cookie = 0  # Initialise le nombre de cookies Ã  0
 
-# Lire le contenu du fichier pour rÃ©cupÃ©rer le nombre de cookies prÃ©cÃ©demment enregistrÃ©
-fichier.seek(0)  # Se dÃ©place au dÃ©but du fichier
+
+fichier.seek(0)  # place le curseur au début du fichier
+
 data = fichier.read()
-if data:  # VÃ©rifie si des donnÃ©es ont Ã©tÃ© lues
-    cookie = int(data)  # Convertit les donnÃ©es lues en entier
+if data: 
+    cookie = int(data) 
 
 def onclick():
     global cookie
@@ -22,21 +22,17 @@ def onclick():
     counter.config(text='Nombre de cookies : ' + str(cookie))
 
 def quitandsave():
-    fichier.seek(0)  # Se dÃ©place au dÃ©but du fichier
-    fichier.truncate()  # Efface le contenu du fichier
-    fichier.write(str(cookie))  # Ã‰crit le nouveau nombre de cookies dans le fichier
+    fichier.seek(0) 
+    fichier.truncate()  # permet d'arrondir la valeur
+    fichier.write(str(cookie))
     fenetre.destroy()
 
-menubar = Menu(fenetre)
-menu1 = Menu(menubar, tearoff=0)
-menu1.add_command(label="Quitter et sauvegarder", command=quitandsave)
-menubar.add_cascade(label="Fichier", menu=menu1)
-fenetre.config(menu=menubar)
+fenetre.protocol("WM_DELETE_WINDOW", quitandsave) # permet d'executer la fonction quitandsave lors de la fermeture de la fenetre
 
 counter = Label(fenetre, text='Nombre de cookies : ' + str(cookie))
 counter.pack()    
 
-bouttoncookie=Button(fenetre, image=imagecookie,command=onclick)
+bouttoncookie=Button(fenetre, image=imagecookie, command=onclick)
 bouttoncookie.pack()
 
 fenetre.mainloop()
