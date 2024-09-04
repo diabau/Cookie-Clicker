261b79ff74f3b165d88ff4cdb8c877d1d0b6afb4
Mathyou <andremathyou@gmail.com>
2024-09-04 04:05:09 +0200

    ajout des mamie et du store

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from tkinter import *
 import os
-
+import time
 fenetre = Tk()
-
+fenetre.title("Cookie Clicker")
 fichier = open("save/data.txt", "a+")
-
+fichier_mamie = open("save/mamie.txt", "a+")
 imagecookie = PhotoImage(file="assets/cookie.png")
-
+fenetre.iconbitmap("assets/cookie.ico")
 fenetre.geometry("500x500")
 fenetre.resizable(width=False, height=False)
 
 
 fichier.seek(0)  # place le curseur au début du fichier
+fichier_mamie.seek(0)
 
 data = fichier.read()
-cookie = int(data) 
+cookie = int(data)
+data_mamie = fichier_mamie.read()
+
+def mamie(): 
+    global cookie
+    cookieseconde = 10 * int(data_mamie)
+    cookie += cookieseconde
+    counter.config(text='Nombre de cookies : ' + str(cookie))
+    fenetre.after(10000, mamie)
+    
 
 def onclick():
     global cookie
@@ -26,7 +36,10 @@ def quitandsave():
     fichier.seek(0) 
     fichier.truncate()  # permet d'arrondir la valeur
     fichier.write(str(cookie))
+    fichier.close()
+    fichier_mamie.close()
     fenetre.destroy()
+    
 
 def store():
     quitandsave()
@@ -41,9 +54,15 @@ counter.pack()
 bouttoncookie=Button(fenetre, image=imagecookie, command=onclick)
 bouttoncookie.pack()
 
+counter_mamie = Label(fenetre, text='Nombre de mamie : ' + str(data_mamie))
+counter_mamie.pack()
+counter_mamie.place(x=0, y=475)
+
 bouttonstore=Button(fenetre, text="store", command=store)
 bouttonstore.pack()
 bouttonstore.place(x=463, y=475)
 
+fenetre.after(10000, mamie)
 fenetre.mainloop()
 fichier.close()
+fichier_mamie.close()
--- store.py
+++ store.py
@@ -1,6 +1,73 @@
+import os
 from tkinter import *
 
-fenetre = Tk()
-fenetre.geometry("500x500")
-fenetre.mainloop()
+root = Tk()
+root.geometry("500x500")
+root.resizable(width=False, height=False)
+imagemamie = PhotoImage(file="assets/mamie.png")
+root.title("Store")
+root.iconbitmap("assets/cookie.ico")
 
+
+fichier = open("save/data.txt", "a+")
+fichier.seek(0)  # place le curseur au début du fichier
+cookie = int(fichier.read())
+
+
+
+
+fichier_mamie = open("save/mamie.txt", "a+")
+fichier_mamie.seek(0)
+data_mamie = fichier_mamie.read()
+data_mamie = int(data_mamie)
+
+def mamie(): 
+    global cookie
+    cookieseconde = 10 * int(data_mamie)
+    cookie += cookieseconde
+    counter.config(text='Nombre de cookies : ' + str(cookie))
+    root.after(10000, mamie)
+
+def achat():
+    global cookie
+    global data_mamie
+    if cookie >= 500:
+        cookie -= 500
+        data_mamie += 1
+        counter.config(text='Nombre de cookies : ' + str(cookie))
+        counter_mamie.config(text='Nombre de mamie : ' + str(data_mamie))
+
+def save():
+    fichier_mamie = open("save/mamie.txt", "a+")
+    fichier = open("save/data.txt", "a+")
+    fichier.seek(0) 
+    fichier.truncate()  # permet d'arrondir la valeur
+    fichier.write(str(cookie))
+    fichier_mamie.seek(0) 
+    fichier_mamie.truncate()  # permet d'arrondir la valeur
+    fichier_mamie.write(str(data_mamie))
+    fichier.close()
+    fichier_mamie.close()
+    root.destroy()
+    os.system('python main.py')
+    
+root.protocol("WM_DELETE_WINDOW", save) # permet d'executer la fonction quitandsave lors de la fermeture de la fenetre
+
+
+
+counter = Label(root, text='Nombre de cookies : ' + str(cookie))
+counter.pack()
+
+counter_mamie = Label(root, text='Nombre de mamie : ' + str(data_mamie))
+counter_mamie.pack()    
+
+button_mamie=Button(root, image=imagemamie, command=achat)
+Label(root, text='Acheté une mamie qui vous fera 10 cookie en 10 seconde\n 500 cookie').pack()
+button_mamie.pack()
+
+
+
+root.after(10000, mamie)
+fichier.close()
+fichier_mamie.close()
+root.mainloop()
